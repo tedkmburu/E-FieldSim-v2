b1d69c50a9787c7cf28b69ad60986b55322b5a8a
tedkmburu <44197881+tedkmburu@users.noreply.github.com>
2021-06-13 00:01:06 -0400

    asdf

--- conductors/fieldVectors.js
+++ conductors/fieldVectors.js
@@ -3,42 +3,74 @@ function createFieldVectors()
     let canvas = foreGroundCanvas;
     fieldVectors = [];
 
-    charges.forEach(charge => {
+    // charges.forEach(charge => {
 
-        let reach = ((Math.abs(charge.charge) + 4) * gridSize)
-        let startX = charge.position.x - reach
-        let startY = charge.position.y - reach
+    //     let reach = ((Math.abs(charge.charge) + 4) * gridSize)
+    //     let startX = charge.position.x - reach
+    //     let startY = charge.position.y - reach
 
-        let endX = charge.position.x + reach
-        let endY = charge.position.y + reach
+    //     let endX = charge.position.x + reach
+    //     let endY = charge.position.y + reach
+
+    //     for (let y = startY; y < endY; y += gridSize) 
+    //     {
+    //         for (let x = startX; x < endX; x += gridSize)
+    //         {
+    //             let arrowLocation = roundVectorToNearestGrid(canvas.createVector(x, y)); // this is the starting location of all the field vectors 
+
+    //             let noChargesNearby = !charges.some(charge => { // if no charges are very clode to the vector, it will be drawn
+    //                 return p5.Vector.dist(arrowLocation, charge.position) < chargeDiameter
+    //             })
+                
+    //             let isInsideConductor = conductors.some(conductor => {
+    //                 return pointIsInsideRect(arrowLocation, conductor)
+    //             }) 
+        
+    //             if (noChargesNearby && !isInsideConductor)
+    //             {
+    //                 // this field vector object is added to an array and everything in the array
+    //                 // will be stored in a variable so they don't need to be recalculated every frame
+    //                 let forceVector = netForceAtPoint(arrowLocation).div(fieldVectorScale);
+
+    //                 fieldVectors.push(new FieldVector(arrowLocation, forceVector)); 
+    //             }
+    //         }
+    //     }
+        
+    // });
+
+    
+    conductors.forEach(conductor => {
+        let reach = (20) * gridSize
+        let startX = conductor.position.x - reach
+        let startY = conductor.position.y - reach
+
+        let endX = conductor.position.x + reach
+        let endY = conductor.position.y + reach
 
         for (let y = startY; y < endY; y += gridSize) 
         {
             for (let x = startX; x < endX; x += gridSize)
             {
                 let arrowLocation = roundVectorToNearestGrid(canvas.createVector(x, y)); // this is the starting location of all the field vectors 
-
-                let noChargesNearby = !charges.some(charge => { // if no charges are very clode to the vector, it will be drawn
-                    return p5.Vector.dist(arrowLocation, charge.position) < chargeDiameter
-                })
+                
                 
                 let isInsideConductor = conductors.some(conductor => {
-                    return pointIsInsideRect(arrowLocation, conductor)
+                    let conductorRect = {position: p5.Vector.sub(conductor.position, canvas.createVector(50, 50)), width: conductor.width + 50, height: conductor.height + 50};
+                    return pointIsInsideRect(arrowLocation, conductorRect)
                 }) 
         
-                if (noChargesNearby && !isInsideConductor)
+                if (!isInsideConductor)
                 {
                     // this field vector object is added to an array and everything in the array
                     // will be stored in a variable so they don't need to be recalculated every frame
-                    let forceVector = netForceAtPoint(arrowLocation).div(fieldVectorScale);
+                    let forceVector = netForceAtPoint(arrowLocation);
 
                     fieldVectors.push(new FieldVector(arrowLocation, forceVector)); 
                 }
             }
         }
-        
-    });
-    
+    })
     // for (let y = 0; y < height; y += gridSize) 
     // {
     //   for (let x = 0; x < width - sidePanelWidth; x += gridSize)
